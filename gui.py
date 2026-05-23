28f64495dfc22e9735777df8e25e044ff366a223
Jan Konczal <JanKonczal33@wp.pl>
2026-05-23 16:47:59 +0200

    dodanie etykiety nad oknem czatu wyświetlającej informacje o zalogowanym użytkowniku. Kliklnięcie w użytkownika/pokój na liście po prawej stronie skutkuje utworzeniem nowego czatu i dodanie go do listy czatów po lewej. Zmiana funkcji append_message: wiadomości dochodzą do użytkownika nawet jeśli czat nadawcy nie jest aktywny.

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@ class ChatGUI():
         self.root.title("Lan Chat App")
         self.client = client
         self.q = queue
+        self.nick = ""
 
         self.current_chat = {"type":"PM","name":"self"}
         self.chats = {}
@@ -53,8 +54,12 @@ class ChatGUI():
         self.room_list.bind("<<ListboxSelect>>", self.on_room_select)
 
     def build_center_panel(self):
+        self.chat_box_upper_label = tk.Label(self.center_frame)
+        self.chat_box_upper_label.pack()
+
         self.chat_box_label = tk.Label(self.center_frame,text="No chat selected")
         self.chat_box_label.pack()
+
 
         self.chat_box = ScrolledText(self.center_frame)
         self.chat_box.pack(fill="both", expand=True)
@@ -91,6 +96,9 @@ class ChatGUI():
         self.users_list.pack(fill="both", expand=True)
         self.rooms_list_right.pack(fill="both", expand=True)
 
+        self.users_list.bind("<<ListboxSelect>>", self.on_user_select)
+        self.rooms_list_right.bind("<<ListboxSelect>>", self.on_room_right_select)
+
     def show_login_popup(self):
         self.login_popup = tk.Toplevel(self.root)
         self.login_popup.title("Login")
@@ -114,6 +122,7 @@ class ChatGUI():
             self.nick = nick
             self.login_popup.destroy()
             self.root.deiconify()
+            self.chat_box_upper_label.config(text=f"User: {self.nick}")
         elif result == "ERROR|Nick is empty":
             tk.messagebox.showerror("Error","Nick is empty")
         elif result == "ERROR|Nick is already used":
@@ -128,14 +137,18 @@ class ChatGUI():
         msg = self.input_entry.get()
         if self.current_chat.get("type",None) == "PM":
             message_to_send = "PM" + "|" + self.current_chat.get("name","Unknown") + "|" + msg
-            #self.client.send_message(message_to_send)
+            print(message_to_send)
+            print(self.current_chat)
+            self.client.send_message(message_to_send)
             gui_msg = self.nick + ":" + " " + msg
             key = f"{self.current_chat['type']}|{self.current_chat['name']}"
             self.append_message(key,gui_msg)
             self.input_entry.delete(0, "end")
         elif self.current_chat.get("type",None) == "ROOM":
             message_to_send = "ROOM" + "|"+ self.current_chat.get("name","Unknown") + "|" +  msg
-            #self.client.send_message(message_to_send)
+            print(message_to_send)
+            print(self.current_chat)
+            self.client.send_message(message_to_send)
             gui_msg = self.nick + ":" + " " + msg
             key = f"{self.current_chat['type']}|{self.current_chat['name']}"
             self.append_message(key, gui_msg)
@@ -144,7 +157,17 @@ class ChatGUI():
     def append_message(self,key,msg):
         self.ensure_chat_exists(key)
         self.chats[key].append(msg)
-        self.refresh_chat()
+        current_key = self.create_key(self.current_chat["type"],self.current_chat["name"])
+        if key == current_key:
+            self.refresh_chat()
+        else:
+            parts = key.split("|",maxsplit=1)
+            name = parts[1]
+            existing_users = self.pm_list.get(0, "end")
+            if name not in existing_users:
+                self.pm_list.insert("end", name)
+
+
 
     def on_pm_select(self,event):
         selection = self.pm_list.curselection()
@@ -153,8 +176,7 @@ class ChatGUI():
 
         index = selection[0]
         name = self.pm_list.get(index)
-        self.current_chat = {"type":"PM","name":name}
-        self.refresh_chat()
+        self.switch_chat(chat_type="PM",name=name)
 
     def on_room_select(self,event):
         selection = self.room_list.curselection()
@@ -162,22 +184,46 @@ class ChatGUI():
             return
         index = selection[0]
         name = self.room_list.get(index)
-        self.current_chat = {"type":"ROOM","name":name}
-        self.refresh_chat()
+        self.switch_chat(chat_type="ROOM",name=name)
+
+    def on_user_select(self,event):
+        selection = self.users_list.curselection()
+        if not selection:
+            return
+        index = selection[0]
+        name = self.users_list.get(index)
+        existing_users = self.pm_list.get(0,"end")
+        if  name not in existing_users:
+            self.pm_list.insert("end",name)
+
+        self.switch_chat(chat_type="PM",name=name)
+
+    def on_room_right_select(self,event):
+        selection = self.rooms_list_right.curselection()
+        if not selection:
+            return
+        index = selection[0]
+        name = self.rooms_list_right.get(index)
+        existing_rooms = self.room_list.get(0,"end")
+        if name not in existing_rooms:
+            self.room_list.insert("end",name)
+
+        self.client.send_message(f"/join|{name}")
+        self.switch_chat(chat_type="ROOM",name=name)
+
+
 
     def switch_chat(self,chat_type,name):
-        key = f"{chat_type}|{name}"
+        key = self.create_key(chat_type,name)
         self.ensure_chat_exists(key)
         self.current_chat = {"type":chat_type,"name":name}
         self.refresh_chat()
-
 
     def refresh_chat(self):
         self.chat_box_label.config(text=f"{self.current_chat['type']}|{self.current_chat['name']}")
         self.chat_box.config(state="normal")
         self.chat_box.delete("1.0","end")
-
-        key= f"{self.current_chat['type']}|{self.current_chat['name']}"
+        key = self.create_key(self.current_chat['type'],self.current_chat["name"])
         for msg in self.chats.get(key,[]):
             self.chat_box.insert("end", msg + "\n")
 
@@ -186,6 +232,10 @@ class ChatGUI():
     def ensure_chat_exists(self,key):
         if key not in self.chats:
             self.chats[key] = []
+
+    def create_key(self,chat_type,name):
+        key= f"{chat_type}|{name}"
+        return key
 
     def process_queue(self):
         while not self.q.empty():
@@ -197,7 +247,7 @@ class ChatGUI():
                 name = message[1]
                 content = message[2]
                 timestamp = message[3]
-                key= f"{chat_type}|{name}"
+                key = self.create_key(chat_type,name)
                 formatted_message = f"{timestamp}|{name}: {content}"
                 self.append_message(key,formatted_message)
             elif chat_type=="ROOM":
@@ -205,7 +255,7 @@ class ChatGUI():
                 sender_nick = message[2]
                 content = message[3]
                 timestamp = message[4]
-                key = f"{chat_type}|{room_name}"
+                key = self.create_key(chat_type,room_name)
                 formatted_message = f"{timestamp}|{room_name}|{sender_nick}: {content}"
                 self.append_message(key,formatted_message)
             elif chat_type=="USERS_UPDATE":
@@ -215,9 +265,9 @@ class ChatGUI():
                     self.users_list.insert("end", u)
             elif chat_type=="ROOMS_UPDATE":
                 rooms = message[1]
-                self.room_list.delete(0, "end")
+                self.rooms_list_right.delete(0, "end")
                 for r in rooms:
-                    self.room_list.insert("end", r)
+                    self.rooms_list_right.insert("end", r)
 
     def start_queue_loop(self):
         self.process_queue()
